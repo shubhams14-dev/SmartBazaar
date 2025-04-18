79968c2aec65b2986d48a35128713d3a114d7814
Shubham Sandip Salunke <shubhamsalunke1401@gmail.com>
2025-04-18 02:29:26 -0400

    Update Cart.jsx

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -19,7 +19,7 @@ const Cart = () => {
   }, [productData]);
 
   const payment = async (token) => {
-    await axios.post("https://SmartBazaar-api-backend/pay", {
+    await axios.post("https://smartbazaar-backend.onrender.com/pay", {
       amount: totalAmt * 100,
       token: token,
     });
